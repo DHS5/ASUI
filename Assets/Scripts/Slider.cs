f10650850a4e8a1f642a15fac3db792be77ce053
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2024-06-16 16:54:55 +0200

    Slider editor + runtime tests

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -4,10 +4,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Dhs5.ASUI
 {
-    public class Slider : Selectable, IDragHandler, IInitializePotentialDragHandler
+    public class Slider : Selectable, IDragHandler, IInitializePotentialDragHandler, IEndDragHandler
     {
         #region Axis
 
@@ -31,16 +37,16 @@ namespace Dhs5.ASUI
 
         [SerializeField] private float m_value;
         [SerializeField] private float m_minValue;
-        [SerializeField] private float m_maxValue;
+        [SerializeField] private float m_maxValue = 1f;
 
         [SerializeField] private bool m_wholeNumbers;
         [SerializeField] private Direction m_direction;
         [SerializeField] private bool m_warpOnClick;
 
-        [SerializeField] private RectTransform m_fillContainer;
-        [SerializeField] private RectTransform m_fillRect;
+        [SerializeField, HideInInspector] private RectTransform m_fillContainer;
+        [SerializeField, HideInInspector] private RectTransform m_fillRect;
         [SerializeField] private Image m_fillImage;
-        [SerializeField] private RectTransform m_handleContainer;
+        [SerializeField, HideInInspector] private RectTransform m_handleContainer;
         [SerializeField] private RectTransform m_handle;
 
         private DrivenRectTransformTracker m_tracker;
@@ -287,13 +293,18 @@ namespace Dhs5.ASUI
         #region Interfaces
 
         private Vector2 m_offset;
+        private bool m_dragIsValid;
 
         public void OnDrag(PointerEventData eventData)
         {
-            if (CanDrag(eventData))
+            if (CanDrag(eventData) && m_dragIsValid)
             {
                 UpdateDrag(eventData);
             }
+        }
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            m_dragIsValid = false;
         }
 
         public override void OnPointerDown(PointerEventData eventData)
@@ -305,6 +316,7 @@ namespace Dhs5.ASUI
             m_offset = Vector2.zero;
             if (m_handleContainer != null && RectTransformUtility.RectangleContainsScreenPoint(m_handle, eventData.pointerPressRaycast.screenPosition, eventData.enterEventCamera))
             {
+                m_dragIsValid = true;
                 if (RectTransformUtility.ScreenPointToLocalPointInRectangle(m_handle, eventData.pointerPressRaycast.screenPosition, eventData.pressEventCamera, out var localPoint))
                 {
                     m_offset = localPoint;
@@ -313,6 +325,11 @@ namespace Dhs5.ASUI
             else if (WarpOnClick)
             {
                 UpdateDrag(eventData);
+                m_dragIsValid = true;
+            }
+            else
+            {
+                m_dragIsValid = false;
             }
         }
 
@@ -363,11 +380,87 @@ namespace Dhs5.ASUI
         {
             base.OnValidate();
 
-            UpdateRectReferences();
+            UpdateVisuals();
         }
 
 #endif
 
         #endregion
     }
+
+    #region Editor
+
+#if UNITY_EDITOR
+
+    [CustomEditor(typeof(Slider))]
+    public class SliderEditor : SelectableEditor
+    {
+        protected SerializedProperty p_value;
+        protected SerializedProperty p_minValue;
+        protected SerializedProperty p_maxValue;
+
+        protected SerializedProperty p_wholeNumbers;
+        protected SerializedProperty p_direction;
+        protected SerializedProperty p_warpOnClick;
+
+        protected SerializedProperty p_fillImage;
+        protected SerializedProperty p_handle;
+
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            p_value = serializedObject.FindProperty("m_value");
+            p_minValue = serializedObject.FindProperty("m_minValue");
+            p_maxValue = serializedObject.FindProperty("m_maxValue");
+
+            p_wholeNumbers = serializedObject.FindProperty("m_wholeNumbers");
+            p_direction = serializedObject.FindProperty("m_direction");
+            p_warpOnClick = serializedObject.FindProperty("m_warpOnClick");
+
+            p_fillImage = serializedObject.FindProperty("m_fillImage");
+            p_handle = serializedObject.FindProperty("m_handle");
+        }
+
+        protected override void OnChildGUI()
+        {
+            EditorGUILayout.Space(15f);
+
+            EditorGUILayout.LabelField("Slider", EditorStyles.boldLabel);
+
+            EditorGUILayout.PropertyField(p_wholeNumbers);
+            EditorGUILayout.PropertyField(p_direction);
+            EditorGUILayout.PropertyField(p_warpOnClick);
+
+            EditorGUILayout.Space(10f);
+
+            bool wholeNumbers = p_wholeNumbers.boolValue;
+
+            if (wholeNumbers)
+            {
+                p_value.floatValue = EditorGUILayout.IntSlider("Value", (int)p_value.floatValue, (int)p_minValue.floatValue, (int)p_maxValue.floatValue);
+                EditorGUILayout.PropertyField(p_minValue);
+                p_minValue.floatValue = Mathf.RoundToInt(p_minValue.floatValue);
+                EditorGUILayout.PropertyField(p_maxValue);
+                p_maxValue.floatValue = Mathf.RoundToInt(Mathf.Max(p_maxValue.floatValue, p_minValue.floatValue + 1f));
+            }
+            else
+            {
+                EditorGUILayout.Slider(p_value, p_minValue.floatValue, p_maxValue.floatValue);
+                EditorGUILayout.PropertyField(p_minValue);
+                EditorGUILayout.PropertyField(p_maxValue);
+                p_maxValue.floatValue = Mathf.Max(p_maxValue.floatValue, p_minValue.floatValue + 0.01f);
+            }
+
+            EditorGUILayout.Space(10f);
+
+            EditorGUILayout.PropertyField(p_fillImage);
+            EditorGUILayout.PropertyField(p_handle);
+        }
+    }
+
+#endif
+
+    #endregion
 }
